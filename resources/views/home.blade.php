a256a942d82d73baf6021c676969d6652c95d851
luisangel861 <lagm861@hotmail.com>
2021-03-02 15:11:06 -0600

    Primera Versión DOCumenti

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}Listado de Lotes</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Agregar Control para Listado de Lotes!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
