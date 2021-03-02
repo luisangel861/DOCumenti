7f6ea696c0e0f169b19b7d176faa50c72586e71c
luisangel861 <lagm861@hotmail.com>
2021-03-02 16:08:36 -0600

    add LogoDocumenti

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,10 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">            
+            <div class="container">                            
                 <a class="navbar-brand" href="{{ url('/') }}">                    
-                    {{ config('app.name', 'DOCumenti') }}
+                    <!-- {{ config('app.name', 'DOCumenti') }} -->
+                    <img src="{{asset('images/Logo_DOCUMENTI.png')}}" width="90" alt="" loading="lazy  ">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
