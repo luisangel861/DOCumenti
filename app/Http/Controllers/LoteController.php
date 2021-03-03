0a1cc23273f12dd9ace92d611267450c27b9c429
luisangel861 <lagm861@hotmail.com>
2021-03-03 13:58:56 -0600

    Ajuste del login, y ajuste de listado y guardado de lotes con el layaout, y agregar funcionalidad del listado de lote

--- app/Http/Controllers/LoteController.php
+++ app/Http/Controllers/LoteController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Lote;
 
 class LoteController extends Controller
 {
     public function mostrar_lotes(){
-    	return view('listado_lotes');
+
+        $Lotes = Lote::all();
+        
+    	return view('listado_lotes',['Lotes'=>$Lotes]);
     }
     public function mostrar_registro(){
     	return view('mostrar_registro');
